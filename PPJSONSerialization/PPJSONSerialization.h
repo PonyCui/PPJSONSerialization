a5ec7bec8644822d94925c1818be55d39e13b3f0
PonyCui <cuiminghui@yy.com>
2015-06-30 17:44:19 +0800

    Initial Commit

---
+++ PPJSONSerialization/PPJSONSerialization.h
@@ -0,0 +1,19 @@
+//
+//  PPJSONSerialization.h
+//  PPJSONSerialization
+//
+//  Created by 崔 明辉 on 15/6/30.
+//  Copyright (c) 2015年 PonyCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPJSONSerialization.
+FOUNDATION_EXPORT double PPJSONSerializationVersionNumber;
+
+//! Project version string for PPJSONSerialization.
+FOUNDATION_EXPORT const unsigned char PPJSONSerializationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPJSONSerialization/PublicHeader.h>
+
+
